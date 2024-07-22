b9774d42438f35ea56087bda09b4a07d16dff3c3
a7medsaber10 <ahmedsa2522003@gmail.com>
2024-07-23 01:02:26 +0300

    Binary operators

---
+++ Demo/Program.cs
@@ -0,0 +1,15 @@
+﻿using Demo.Operator_Overloading;
+
+namespace Demo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Complex C1 = new Complex() { Real = 3, Imag = 4};
+            Complex C2 = new Complex() { Real = 1, Imag = 2 };
+
+            Complex C3 = C1 + C2;
+        }
+    }
+}
